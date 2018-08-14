c8f8c7c220710da5172cfd4244899ce86fd1ef71
Starcev Michail <starcev.m@skbkontur.ru>
2018-08-14 11:06:57 +0500

    add CSharpSyntaxFactory generator

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -55,7 +56,7 @@ namespace ConsoleApp3
 			var lambda2 = BuildDictionaryToTypeExpression(typeof(Person));
 
 			//            SaveLambda(lambda2);
-			RoslynExampleGenerateMethod(typeof(Person));
+			RoslynExampleGenerateMethodWithCSharpSyntaxFactory(typeof(Person));
 			var person = lambda2.Compile()(dictionary);
 			var personReflection = MapDictionaryToTypeReflection(typeof(Person), dictionary);
 			var personHM = MapDictionaryToTypeHandMade(dictionary);
@@ -64,10 +65,30 @@ namespace ConsoleApp3
 
 		public static void RoslynExampleGenerateMethodWithCSharpSyntaxFactory(Type type)
 		{
+			var ifBlock = new StringBuilder();
+			foreach (var property in type.GetProperties())
+			{
+				ifBlock.AppendFormat(@"if (dictionary.TryGetValue(""{0}"", out value))
+				{{
+					target.{0} = ({1})value;
+				}}
+				", property.Name, property.PropertyType.Name);
+			}
+
+			var code = $@"public static object MapDictionaryToType1(Dictionary<string, object> dictionary)
+			{{
+				var target = new {type.Name}();
+				object value;
+				return person;
+				{ifBlock}
+			}}
+			";
+			Console.WriteLine(code);
 		}
 
 		public static void RoslynExampleGenerateMethod(Type type)
 		{
+			//var person = new Person();
 			var typeIdentifier = SyntaxFactory.IdentifierName(type.Name);
 			var personDeclaration = SyntaxFactory.LocalDeclarationStatement(
 				SyntaxFactory.VariableDeclaration(typeIdentifier)
@@ -80,10 +101,11 @@ namespace ConsoleApp3
 							)
 					)
 			);
+			//object value;
 			var outValueDeclaration = SyntaxFactory.LocalDeclarationStatement(
 				SyntaxFactory.VariableDeclaration(SyntaxFactory.IdentifierName("object"))
 					.AddVariables(SyntaxFactory.VariableDeclarator("value")));
-
+			// person.Id = (int) value;
 			var assign = SyntaxFactory.ExpressionStatement(
 				SyntaxFactory.AssignmentExpression(
 					SyntaxKind.SimpleAssignmentExpression,
@@ -93,12 +115,12 @@ namespace ConsoleApp3
 						SyntaxFactory.IdentifierName("Id")
 					),
 					SyntaxFactory.CastExpression(
-						SyntaxFactory.ParseTypeName("string"),
+						SyntaxFactory.ParseTypeName("int"),
 						SyntaxFactory.IdentifierName("value")
 					)
 				)
 			);
-
+			//if (dictionary.TryGetValue("Id", out value))
 			var ifBlock = SyntaxFactory.IfStatement(
 				SyntaxFactory.InvocationExpression(
 					SyntaxFactory.MemberAccessExpression(
@@ -127,113 +149,6 @@ namespace ConsoleApp3
 			var code = methodDeclaration
 				.NormalizeWhitespace()
 				.ToFullString();
-			Console.WriteLine(code);
-
-			//			var memberaccess = SyntaxFactory.MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression,
-			//				SyntaxFactory.IdentifierName("dictionary"), SyntaxFactory.IdentifierName("TryGetValue"));
-			//
-			//
-			//			var identifier = SyntaxFactory.IdentifierName("MyClass");
-			//			var objectCreationExpression =
-			//				SyntaxFactory.ObjectCreationExpression(identifier, SyntaxFactory.ArgumentList(), null);
-			//			var equalsValueClause = SyntaxFactory.EqualsValueClause(objectCreationExpression);
-			//
-			//
-			//			var argument = SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression,
-			//				SyntaxFactory.Literal("Id")));
-			//			var argument2 = SyntaxFactory.Argument(null,
-			//				SyntaxFactory.Token(SyntaxKind.OutKeyword),
-			//				SyntaxFactory.IdentifierName("value"));
-			//			var argumentList = SyntaxFactory.SeparatedList(new[] {argument, argument2});
-			//
-			//			var syntax = SyntaxFactory.Block(
-			//				SyntaxFactory.LocalDeclarationStatement(
-			//					SyntaxFactory.VariableDeclaration(SyntaxFactory.ParseTypeName("object"))
-			//						.AddVariables(SyntaxFactory.VariableDeclarator("value"))
-			//				),
-			//				SyntaxFactory.IfStatement( //invocationExpression
-			//					condition: SyntaxFactory.InvocationExpression(memberaccess,
-			//						SyntaxFactory.ArgumentList(argumentList)),
-			//					statement: SyntaxFactory.Block(
-			//						//					person.Id = (int) value;
-			//					)
-			//				)
-			////			,
-			////			SyntaxFactory.VariableDeclaration(SyntaxFactory.ParseTypeName("object"))
-			////				.AddVariables(SyntaxFactory.VariableDeclarator("value"))
-			//			);
-			//
-			//			var methodDeclaration = SyntaxFactory
-			//				.MethodDeclaration(SyntaxFactory.ParseTypeName("object"), "Foo")
-			//				.AddParameterListParameters(
-			//					SyntaxFactory.Parameter(
-			//							SyntaxFactory.Identifier("dictionary"))
-			//						.WithType(SyntaxFactory.ParseTypeName("Dictionary<string, object>")))
-			//				.AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword),
-			//					SyntaxFactory.Token(SyntaxKind.StaticKeyword))
-			//				.WithBody(syntax);
-			//
-			//
-			//			var code = methodDeclaration
-			//				.NormalizeWhitespace()
-			//				.ToFullString();
-			//
-			//			// Output new code to the console.
-			//			Console.WriteLine(code);
-		}
-
-		public static void RoslynExampleGenerateClassAndMethod()
-		{
-			// Create a namespace: (namespace CodeGenerationSample)
-			var @namespace = SyntaxFactory.NamespaceDeclaration(SyntaxFactory.ParseName("CodeGenerationSample"));
-
-			// Add System using statement: (using System)
-			@namespace = @namespace.AddUsings(SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("System")));
-
-			//  Create a class: (class Order)
-			var classDeclaration = SyntaxFactory.ClassDeclaration("Order");
-
-			// Add the public modifier: (public class Order)
-			classDeclaration = classDeclaration.AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword));
-
-			// Create a string variable: (bool canceled;)
-			var variableDeclaration = SyntaxFactory.VariableDeclaration(SyntaxFactory.ParseTypeName("bool"))
-				.AddVariables(SyntaxFactory.VariableDeclarator("canceled"));
-
-			// Create a field declaration: (private bool canceled;)
-			var fieldDeclaration = SyntaxFactory.FieldDeclaration(variableDeclaration)
-				.AddModifiers(SyntaxFactory.Token(SyntaxKind.PrivateKeyword));
-
-			// Create a Property: (public int Quantity { get; set; })
-			var propertyDeclaration = SyntaxFactory.PropertyDeclaration(SyntaxFactory.ParseTypeName("int"), "Id")
-				.AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
-				.AddAccessorListAccessors(
-					SyntaxFactory.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration)
-						.WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken)),
-					SyntaxFactory.AccessorDeclaration(SyntaxKind.SetAccessorDeclaration)
-						.WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken)));
-
-			// Create a stament with the body of a method.
-			var syntax = SyntaxFactory.ParseStatement("canceled = true;");
-
-			// Create a method
-			var methodDeclaration = SyntaxFactory
-				.MethodDeclaration(SyntaxFactory.ParseTypeName("void"), "MarkAsCanceled")
-				.AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
-				.WithBody(SyntaxFactory.Block(syntax));
-
-			// Add the field, the property and method to the class.
-			classDeclaration = classDeclaration.AddMembers(fieldDeclaration, propertyDeclaration, methodDeclaration);
-
-			// Add the class to the namespace.
-			@namespace = @namespace.AddMembers(classDeclaration);
-
-			// Normalize and get code as string.
-			var code = @namespace
-				.NormalizeWhitespace()
-				.ToFullString();
-
-			// Output new code to the console.
 			Console.WriteLine(code);
 		}
 
